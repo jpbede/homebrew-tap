8c0bd1aec1020cdacc23b037e88a8e13b49a26c1
Jan-Philipp Benecke <github@bnck.me>
2020-10-29 10:15:07 +0100

    Update eop-header.rb

--- Formula/eop-header.rb
+++ Formula/eop-header.rb
@@ -7,11 +7,11 @@ class EopHeader < Formula
 
   if OS.mac?
     url "https://github.com/jpbede/eop-header/releases/download/v0.0.2/eop-header_0.0.2_Darwin_x86_64.tar.gz"
-    sha256 "7438c50993f634779dc8e3cc41a3cb9076b6d4b4e47945d69ff685170fdb3bb6"
+    sha256 "5637d386bc8b259bedfa7ba9b7cf840f52f4d93ee2913e66c6d15edcd1ac379a"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/jpbede/eop-header/releases/download/v0.0.2/eop-header_0.0.2_Linux_x86_64.tar.gz"
-      sha256 "b0df928f162168ce058663bfeffd01d91ec4cc6f1e1a5fc1e1c836efd4f38da9"
+      sha256 "c428319786c513db92775a310b0aba5e0b4706c308cacf55d3b017858a09e827"
     end
   end
 
