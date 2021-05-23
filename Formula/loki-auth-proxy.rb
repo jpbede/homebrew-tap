aabad9c503cc64a2e4841cc5410c9e1d5c2648a0
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-23 20:55:15 +0200

    Brew formula update for loki-auth-proxy version v0.0.3

--- Formula/loki-auth-proxy.rb
+++ Formula/loki-auth-proxy.rb
@@ -5,24 +5,24 @@
 class LokiAuthProxy < Formula
   desc "Stateless Grafana Loki authentication proxy"
   homepage "https://github.com/jpbede/loki-auth-proxy"
-  version "0.0.2"
+  version "0.0.3"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/jpbede/loki-auth-proxy/releases/download/v0.0.2/loki-auth-proxy_0.0.2_Darwin_x86_64.tar.gz"
-    sha256 "f232295c8d101c94192e6768d17d1431442ea2ee222fc2c02d99dd3c5a2a82b6"
+    url "https://github.com/jpbede/loki-auth-proxy/releases/download/v0.0.3/loki-auth-proxy_0.0.3_Darwin_x86_64.tar.gz"
+    sha256 "1e0a3d9ba0081ba0defbbb7e68af38b8acfef75d9e3bea21ea677fe84942d8a7"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/jpbede/loki-auth-proxy/releases/download/v0.0.2/loki-auth-proxy_0.0.2_Darwin_arm64.tar.gz"
-    sha256 "c4065caed7f1a3968f2744d6041beec6d2c97cec1884382d7b044da3a80a4357"
+    url "https://github.com/jpbede/loki-auth-proxy/releases/download/v0.0.3/loki-auth-proxy_0.0.3_Darwin_arm64.tar.gz"
+    sha256 "3d1c57eb98e794ac60c99522132eea1e2324fd31287546557de6e5bdbc8857ca"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/jpbede/loki-auth-proxy/releases/download/v0.0.2/loki-auth-proxy_0.0.2_Linux_x86_64.tar.gz"
-    sha256 "8b19272225edd6ad81620d1076cccbcabe1ebdeb606eeb6510208a0a1275eab5"
+    url "https://github.com/jpbede/loki-auth-proxy/releases/download/v0.0.3/loki-auth-proxy_0.0.3_Linux_x86_64.tar.gz"
+    sha256 "83326a09ba7eadaeabc8c22095ad617ef8c31bf78a4c68c82dc3f1456d943f74"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/jpbede/loki-auth-proxy/releases/download/v0.0.2/loki-auth-proxy_0.0.2_Linux_arm64.tar.gz"
-    sha256 "6adccb1ad76d80e7ee58d3516ddaeda05ffe0f23dc871b519a142d6cc1fd6ca4"
+    url "https://github.com/jpbede/loki-auth-proxy/releases/download/v0.0.3/loki-auth-proxy_0.0.3_Linux_arm64.tar.gz"
+    sha256 "7f8f79fe84731804142ad6591b6ac6b2f7d5fd0ef123f7e374f38c5e8a915ea4"
   end
 
   def install
